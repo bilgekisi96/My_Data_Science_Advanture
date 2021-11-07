f3880222fa27a941c374667f8baca4eae31ddd41
yavuz selim <yavuzselimozkok@gmail.com>
2021-11-07 16:50:14 +0300

    my_data_science_tutorial

--- Data_Analysis/4.week/Amazon_question.py
+++ Data_Analysis/4.week/Amazon_question.py
@@ -10,8 +10,23 @@ For example, given the following matrix:
 You should print out the following:
 """
 
-
-
+import numpy as np
+mat=np.array([[1,  2,  3,  4,  5],
+              [6,  7,  8,  9,  10],
+              [11, 12, 13, 14, 15],
+              [16, 17, 18, 19, 20],
+              [21, 22, 23, 24, 25]])
+lis,sifir,bos=[j for i in mat for j in i],0,[]
+a,b=mat.shape
+while True:
+    a,b=a-1,b-1
+    degisken,dol = list(mat[sifir])+list(mat[:,b])+list(mat[a][::-1])+list(mat[:,sifir][::-1]),[]
+    sifir += 1
+    for was in degisken:
+        if was in bos:pass
+        else:bos.append(was)
+    if len(bos)==len(lis):break
+print(bos)
 
 
 
